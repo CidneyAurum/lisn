268f37e9c000026fb1f5f37fc35d65de13cf67ed
CidneyAurum <203533003+CidneyAurum@users.noreply.github.com>
2026-09-19 17:37:37 +0800

    release: v0.1.4

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  define: { __APP_VERSION__: JSON.stringify('0.1.3') },
+  define: { __APP_VERSION__: JSON.stringify('0.1.4') },
   main: {
     plugins: [externalizeDepsPlugin()],
     build: {
